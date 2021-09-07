0489b0cd60546814947a70ff0ebd65d1f53bce15
charuka <charuka.18@cse.mrt.ac.lk>
2021-09-07 11:55:28 +0530

    Authorization update

---
+++ src/views/Home/SupervisorDashboard/dashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../../../components/SidePanel/sidePanel";
+
+import TopBar from "../../../components/TopBar/topBar";
+export default function SupervisorDashboard(){
+    
+    
+   
+    return(
+        <div>
+            <Nav>
+                <TopBar/>
+                <h1>Supervisor Dashboard</h1>
+            </Nav>
+        </div>
+    )
+}
